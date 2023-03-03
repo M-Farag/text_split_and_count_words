a9970e37fa1e0ff7b1976ace140519755a41058f
M-Farag <mina.farag@icloud.com>
2023-03-04 01:08:20 +0200

    ADD Word counter

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::io;
-use crate::text_processor::{split};
+use crate::text_processor::{split,words_counter};
 
 pub mod text_processor;
 fn main() {
     let mut user_input:String = String::new();
     println!("Please input your text ?!");
-    
+
     io::stdin().read_line(&mut user_input).expect("Err reading buffer");
     let words = split(user_input.trim());
 
-    for word in words {
-        println!("Word: {}",word);
+    let words_count = words_counter(words);
+
+    println!("Word  count");
+    for (word, count) in words_count
+    {
+        println!("{}    {}",word,count);
     }
 }
 
--- src/text_processor.rs
+++ src/text_processor.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn split(text_input:&str) ->Vec<&str>
 {
     let mut words:Vec<&str> = Vec::new();
@@ -13,4 +15,14 @@ pub fn split(text_input:&str) ->Vec<&str>
     }
     words.push(&text_input[start_index..]);
     words
+}
+
+pub fn words_counter(words:Vec<&str>) -> HashMap<&str,usize>
+{
+    let mut words_count:HashMap<&str,usize> = HashMap::new();
+    for word in words {
+        let counter = words_count.entry(word).or_insert(0);
+        *counter += 1;
+    }
+    words_count
 }
